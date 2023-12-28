0fd083ec1bc94fc9f913b3af47f4b07d46f65c3b
Lê Minh Đức <107402332+leeminhduc2@users.noreply.github.com>
2023-12-28 23:23:57 +0700

    Create leeminhduc2.cpp

---
+++ leeminhduc2.cpp
@@ -0,0 +1,27 @@
+/// DMCS
+#include <bits/stdc++.h>
+#define ll long long
+#define f first
+#define s second
+#define ii pair<int,int>
+#define sz(x) (int) (x).size()
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+template<class T1,class T2> bool maximize(T1 &a,T2 b) {return(a<b ? a=b,1:0);};
+template<class T1,class T2> bool minimize(T1 &a,T2 b) {return(a>b ? a=b,1:0);};
+
+void leeminhduc2()
+{
+    
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tc=1;
+    //cin >> tc;
+    while (tc--)
+    leeminhduc2();
+}
+
